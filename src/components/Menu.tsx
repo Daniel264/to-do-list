3d3d40b741d50561378fd1fe53cbb26ced16ee6f
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-13 00:11:46 +0100

    Add search icon to input field

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { IoSearch } from "react-icons/io5";
 
 const Menu = () => {
   return (
     <>
-      <div className="p-10">
-        <h1 className="font-semibold text-2xl">Menu</h1>
-        <input className="w-max" placeholder="Search" type="text" />
+      <div className="p-10 relative">
+        <h1 className="font-semibold text-2xl pb-5">Menu</h1>
+        <input className="w-max h-7 pl-7 outline-none" placeholder="Search..." type="text" />
+        <IoSearch size={20} className="absolute text-gray-500 top-24  "/>
       </div>
     </>
   );
