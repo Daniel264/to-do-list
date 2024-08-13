d35f223864f7e5c64d68348d9562c37c642b5470
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-14 00:14:33 +0100

    Add icons to each task list element

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -1,23 +1,23 @@
 import { FaTasks } from "react-icons/fa";
-import { IconType } from "react-icons";
 import { Tasks } from "../hooks/useMenu";
-import Menu from "./Menu";
-
+import { BiCalendar } from "react-icons/bi";
+import { CiStickyNote } from "react-icons/ci";
+import { MdUpcoming } from "react-icons/md";
 const MenuList = () => {
   const tasks: Tasks[] = [
-    { id: 1, name: "Upcoming", logo: "" },
+    { id: 1, name: "Upcoming", logo: MdUpcoming },
     { id: 2, name: "Today", logo: FaTasks },
-    { id: 3, name: "Calendar", logo: "" },
-    { id: 4, name: "Sticky Wall", logo: "" },
+    { id: 3, name: "Calendar", logo: BiCalendar },
+    { id: 4, name: "Sticky Wall", logo: CiStickyNote },
   ];
   return (
     <>
       <ul>
         {tasks.map((task) => (
-          <li className="cursor-pointer py-1" key={task.id}>
+          <li className="cursor-pointer py-1 flex text-gray-500 font-medium" key={task.id}>
             {typeof task.logo == "string"
               ? task.logo
-              : task.logo && <task.logo />}
+              : task.logo && <task.logo className="mt-1 mr-2" />}
             {task.name}
           </li>
         ))}
