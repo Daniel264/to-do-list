a19b7a7aef591d242a5c0fb7b802d60192ab660b
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-13 21:46:01 +0100

    Display MenuList in Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,22 @@
 import { IoSearch } from "react-icons/io5";
-
+import MenuList from "./MenuList";
 const Menu = () => {
   return (
     <>
       <div className="p-10 relative">
         <h1 className="font-semibold text-2xl pb-5">Menu</h1>
-        <input className="w-max h-7 pl-7 outline-none" placeholder="Search..." type="text" />
-        <IoSearch size={20} className="absolute text-gray-500 top-24  "/>
+        <input
+          className="w-max h-7 pl-7 outline-none"
+          placeholder="Search..."
+          type="text"
+        />
+        <IoSearch size={20} className="absolute text-gray-500 top-24  " />
       </div>
 
       <div className="p-10">
         <h3 className="font-semibold text-base">TASKS</h3>
-  
+        <MenuList />
       </div>
-
     </>
   );
 };
