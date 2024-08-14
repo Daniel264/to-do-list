95f27fbb82248b695906741f24e28fa03c94ce4e
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-14 20:18:44 +0100

    Add the correct padding to each div

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { IoSearch } from "react-icons/io5";
 import Task from "./Task";
 import List from "./List";
+import Tags from "./Tags";
 const Menu = () => {
   return (
     <>
@@ -14,13 +15,17 @@ const Menu = () => {
         <IoSearch size={20} className="absolute text-gray-500 top-24  " />
       </div>
 
-      <div className="p-10">
+      <div className="px-10 py-3">
         <h3 className="font-semibold text-base">TASKS</h3>
         <Task />
       </div>
-      <div className="p-10">
+      <div className="px-10 py-3">
         <h3 className="font-semibold text-base">LISTS</h3>
         <List />
+      </div>
+      <div className="px-10 py-3">
+        <h3 className="font-semibold">TAGS</h3>
+        <Tags />
       </div>
     </>
   );
---
+++ src/components/Tags.tsx
@@ -0,0 +1,13 @@
+
+
+const Tags = () => {
+  return (
+    <>
+      <button className="btn bg-blue-200">Tag 1</button>
+      <button className="btn bg-red-300">Tag 2</button>
+      <button className="btn bg-gray-200">+ Add Tag</button>
+    </>
+  );
+};
+
+export default Tags;
