69fed2078e2d3179430ad14cb31d9a5fa4313e71
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-14 20:20:49 +0100

    Add Tags component without design

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -3,8 +3,8 @@
 const Tags = () => {
   return (
     <>
-      <button className="btn bg-blue-200">Tag 1</button>
-      <button className="btn bg-red-300">Tag 2</button>
+      <button className="btn bg-blue-200 mr-4">Tag 1</button>
+      <button className="btn bg-red-300 mr-4">Tag 2</button>
       <button className="btn bg-gray-200">+ Add Tag</button>
     </>
   );
