90c8fafcc3b49b1ea28e570ea2940471edb7c3d2
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-15 20:40:23 +0100

    change the grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="grid grid-cols-3">
-        <div className="bg-gray-100">
+      <div className="grid grid-cols-2 h-screen">
+        <div className="w-2/3 bg-gray-100">
           <Menu />
         </div>
-        <div>
+        <div className="w-1/3">
           <Main />
         </div>
-        <div>Aside</div>
       </div>
     </>
   );
