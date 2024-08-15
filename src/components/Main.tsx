f5591e83904cadc645f630773ea7f6fef74c7b7f
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-15 20:32:45 +0100

    Align main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,23 @@
-import { CgAdd } from "react-icons/cg";
+import { MdAdd } from "react-icons/md";
+// import { Lists } from "../hooks/useList";
+// import { BiCalendar } from "react-icons/bi";
 
 const Main = () => {
-    const taskList = [
-
-    ]
+//   const taskList: Lists[] = [
+//     { id: 1, text: "Research content ideas" },
+//     { id: 2, text: "Create a database of Guest Authors" },
+//     {id: 3, text: "Renew Driver's License",logo
+//         : BiCalendar, logo_text: 22-03-22
+//      }
+//   ];
   return (
     <>
-      <h1 className="font-semibold text-5xl">Today</h1>
+      <div className="h-fit flex justify-center">
+          <h1 className="font-semibold text-5xl ">Today</h1>
+      </div>
       <hr className="mt-5" />
-      <p>
-        <CgAdd />
+      <p className="mt-5 flex pl-10">
+        <MdAdd  className="mr-4"/>
         Add New Task
       </p>
     </>
--- src/hooks/useList.ts
+++ src/hooks/useList.ts
@@ -1,5 +1,8 @@
+import { IconType } from "react-icons";
+
 export interface Lists {
   id: number;
   text: string;
-  logo_text: string;
+  logo_text?: string;
+  logo?: IconType;
 }
